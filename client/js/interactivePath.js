5da4cff263b3075b759092f3d25a891d0c5deee2
BuildTools <unconfigured@null.spigotmc.org>
2023-02-26 19:46:50 +0100

    added drag description

--- client/js/interactivePath.js
+++ client/js/interactivePath.js
@@ -2,23 +2,55 @@ import { setCookie } from "./cookies.js";
 import { load } from "./navigation.js";
 import { makeDraggable, makeDropZone } from "./dropzone.js";
 
+export function createInteractivePathElement() {
+	let interactivePathElement = document.createElement("div");
+	interactivePathElement.setAttribute("id", "interactive-path");
+
+	let inputElement = document.createElement("input");
+	inputElement.classList.add("hidden");
+	inputElement.setAttribute("spellcheck", false);
+
+	interactivePathElement.prepend(inputElement);
+
+	interactivePathElement.addEventListener("click", function (event) {
+		let inputElement = this.querySelector("input");
+		inputElement.classList.remove("hidden");
+
+		this.querySelectorAll("div.interactive-path-component").forEach(x => x.classList.add("hidden"));
+
+		let path = getCompleteRelativePathFromInteractivePathComponent(this.querySelector("div.interactive-path-component:last-of-type"));
+		inputElement.value = path;
+		inputElement.select();
+
+		event.stopPropagation();
+	});
+
+	inputElement.addEventListener("keyup", function (e) {
+		if (e.code === "Enter") {
+			window.socket.emit("is-path-valid", this.value, valid => {
+				if (valid) {
+					this.classList.add("hidden");
+					window.history.pushState(this.value, "", this.value);
+					load();
+				} else {
+					console.log("NOT VALID");
+				}
+			});
+		}
+	});
+
+	return interactivePathElement;
+}
+
 export default function setInteractivePath(value) {
 	let interactivePathElement = document.querySelector("#interactive-path");
-	interactivePathElement.replaceChildren();
+	interactivePathElement.querySelectorAll(".interactive-path-component").forEach(x => x.remove());
 
 	let parts = value.match(/(^\/)|([^\/]+)/gim);
 
 	for (let part of parts) {
 		let component = createInteractivePathComponent(part);
 		interactivePathElement.append(component);
-
-		component.querySelector(".value").addEventListener("click", function (event) {
-			let interactivePathComponent = this.closest(".interactive-path-component");
-
-			if (interactivePathElement.lastChild !== interactivePathComponent) {
-				getCompleteRelativePathFromInteractivePathComponent(interactivePathComponent);
-			}
-		});
 	}
 }
 
@@ -29,8 +61,10 @@ function createInteractivePathComponent(value) {
 	interactivePathComponent.querySelector(".value").innerText = value;
 	let completePath = getCompleteRelativePathFromInteractivePathComponent(interactivePathComponent) + value;
 	interactivePathComponent.querySelector(".path").innerText = completePath.endsWith("/") ? completePath : completePath + "/";
-	interactivePathComponent.addEventListener("click", function (e) {
-		let path = getCompleteRelativePathFromInteractivePathComponent(this);
+	interactivePathComponent.querySelector(".value").addEventListener("click", function (e) {
+		e.stopPropagation();
+
+		let path = getCompleteRelativePathFromInteractivePathComponent(this.closest("div.interactive-path-component"));
 		window.history.pushState(path, "", path);
 		load();
 	});
--- client/js/onload.js
+++ client/js/onload.js
@@ -1,9 +1,19 @@
 import { load } from "./navigation.js";
 import registerSocketEventHandlers from "./socketEventHandler.js";
 import { makeDropZone } from "./dropzone.js";
+import { createInteractivePathElement } from "./interactivePath.js";
 
 window.onload = () => {
 	makeDropZone(document.querySelector("#directory-contents #contents"), true);
+
+	if (!document.querySelector("#interactive-path")) {
+		document
+			.querySelector("#control-bar")
+			.insertBefore(
+				createInteractivePathElement(document.querySelector("#control-bar")),
+				document.querySelector("#control-bar #create-folder-icon")
+			);
+	}
 
 	let pathname = window.location.pathname;
 
